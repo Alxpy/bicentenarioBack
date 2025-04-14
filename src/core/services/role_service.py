a2f5700d4bdf9f824c29f18e372938a5be4d31da
alxpy <alxpyk09@gmail.com>
2025-04-14 13:18:23 -0400

    feat: add role management endpoints and services

--- src/core/services/role_service.py
+++ src/core/services/role_service.py
@@ -2,7 +2,7 @@ from src.core.abstractions.services.role_service_abstract import IRolSerice
 from src.core.abstractions.infrastructure.repository.role_repository_abstract import IRolRepository
 from src.core.models.rol_domain import RolDomain
 
-class RolRepository(IRolSerice):
+class RolService(IRolSerice):
     
     def __init__(self, rol_repository: IRolRepository):
         self.rol_repository = rol_repository
