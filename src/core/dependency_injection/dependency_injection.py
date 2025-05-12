0e23c17fb155d4630a04dd8722bc489855dc0b8c
VjSuxo <vjoelsuxod@gmail.com>
2025-05-12 12:36:08 -0400

    fix: update dependency injection to use get_db_connection instead of get_connection

--- src/core/dependency_injection/dependency_injection.py
+++ src/core/dependency_injection/dependency_injection.py
@@ -1,6 +1,6 @@
 from fastapi import Depends
 from src.infrastructure.repository.connection_mail import get_gmail_service
-from src.infrastructure.repository.dependency_injection.dependency_injection import get_connection
+from src.infrastructure.repository.dependency_injection.dependency_injection import get_db_connection
 
 from src.infrastructure.repository.implementations.user_repository import UserRepository
 from src.core.services.user_service import UsuarioService
@@ -85,84 +85,84 @@ from src.infrastructure.repository.implementations.usuario_rol_repository import
 from src.core.services.usuario_rol_service import UsuarioRolService
 
 def build_usuario_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return UsuarioService(UserRepository(connection))
 
 def build_auth_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return AuthService(AuthRepository(connection))
 
 def build_email_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
     connection_email = Depends(get_gmail_service),
 ):
     return EmailService(EmailRepository(connection, connection_email))
 
 def build_categoriaNoticia_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return CategoriaNoticiaService(CategoriaNoticiaRepository(connection))
 
 def build_noticia_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return NoticiaService(NoticiaRepository(connection))
 
 def build_tipoDocumento_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return TipoDocumentoService(TipoDocumentoRepository(connection))
 
 def build_biblioteca_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return BibliotecaService(BibliotecaRepository(connection))
 
 
 def build_ubicacion_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return UbicacionService(UbicacionRepository(connection))
 
 def build_cultura_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return CulturaService(CulturaRepository(connection))
 
 def build_presidente_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return PresidenteService(PresidenteRepository(connection))
 
 def build_rol_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return RolService(RolRepository(connection))
 
 def build_multimedia_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return MultimediaService(MultimediaRepository(connection))
 
 def build_multimedia_historia_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return MultimediaHistoriaService(MultimediaHistoriaRepository(connection))
 
 def build_multimedia_cultura_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
    return MultimediaCulturaService(MultimediaCulturaRepository(connection))
 
 def build_categoria_historia_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return CategoriaHistoriaService(CategoriaHistoriaRepository(connection))
 
 def build_historia_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return HistoriaService(HistoriaRepository(connection))
 
@@ -171,52 +171,52 @@ def build_file_storage_service(
     return FileStorageService()
 
 def build_tipo_evento_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return TipoEventoService(TipoEventoRepository(connection))
 
 def build_evento_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return EventoService(EventoRepository(connection))
 
 def build_usuario_evento_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return UsuarioEventoService(UsuarioEventoRepository(connection))
 
 def build_patrocinador_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return PatrocinadorService(PatrocinadorRepository(connection))
 
 def build_patrocinador_evento_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return PatrocinadorEventoService(PatrocinadorEventoRepository(connection))
 
 def build_agenda_personal_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return AgendaPersonalService(AgendaPersonalRepository(connection))
 
 def build_comentario_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return ComentarioService(ComentarioRepository(connection))
 
 
 def build_comentario_evento_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return ComentarioEventoService(ComentarioEventoRepository(connection))
 
 def build_comentario_biblioteca_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return ComentarioBibliotecaService(ComentarioBibliotecaRepository(connection))
 
 def build_usuario_rol_service(
-    connection = Depends(get_connection),
+    connection = Depends(get_db_connection),
 ):
     return UsuarioRolService(UsuarioRolRepository(connection))
